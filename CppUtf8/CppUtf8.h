aa67da13f24f7b2d0de838a2f58164b1075a3bec
Leundo <daysundoing@outlook.com>
2024-03-13 22:08:12 +0800

    Initial Commit

---
+++ CppUtf8/CppUtf8.h
@@ -0,0 +1,18 @@
+//
+//  CppUtf8.h
+//  CppUtf8
+//
+//  Created by Undo Hatsune on 2024/03/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CppUtf8.
+FOUNDATION_EXPORT double CppUtf8VersionNumber;
+
+//! Project version string for CppUtf8.
+FOUNDATION_EXPORT const unsigned char CppUtf8VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CppUtf8/PublicHeader.h>
+
+
